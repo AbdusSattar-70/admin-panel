449ae107cf6a6a5523378cccea57a3a9264a3708
Abdus Sattar <sasorganicagro@gmail.com>
2023-12-07 15:12:53 +0600

    Add server.js main file

---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const corsOptions = require('./config/corsOptions');
+const { logger } = require('./middleware/logEvents');
+const errorHandler = require('./middleware/errorHandler');
+const verifyJWT = require('./middleware/verifyJWT');
+const cookieParser = require('cookie-parser');
+const credentials = require('./middleware/credentials');
+const PORT = process.env.PORT || 3500;
+
+// custom middleware logger
+app.use(logger);
+
+
+// database connection using mongoose
+const dbConnection = async () => {
+  try {
+    await mongoose.connect(process.env.MONGO_URI);
+    console.log('database connected successfully');
+  } catch (error) {
+    throw new Error(error);
+  }
+};
+dbConnection();
+
+// Handle options credentials check - before CORS!
+// and fetch cookies credentials requirement
+app.use(credentials);
+
+// Cross Origin Resource Sharing
+app.use(cors(corsOptions));
+
+// built-in middleware to handle urlencoded form data
+app.use(express.urlencoded({ extended: true }));
+
+// built-in middleware for json
+app.use(express.json());
+
+//middleware for cookies
+app.use(cookieParser());
+
+// routes
+app.use('/register', require('./routes/register'));
+app.use('/auth', require('./routes/auth'));
+app.use('/refresh', require('./routes/refresh'));
+app.use('/logout', require('./routes/logout'));
+
+app.use(verifyJWT);
+app.use('/users', require('./routes/api/users'));
+
+app.use(errorHandler);
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
